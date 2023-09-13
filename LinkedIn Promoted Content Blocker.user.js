41e9dad4a20e21e69a27039c8fe27ae9b265aea9
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-09-12 22:42:03 -0400

    Add files via upload

---
+++ LinkedIn Promoted Content Blocker.user.js
@@ -0,0 +1,24 @@
+/// ==UserScript==
+// @name         LinkedIn Promoted Content Blocker
+// @namespace    https://www.jabedmiah.com
+// @version      1.0
+// @description  Blocks promoted posts from the LinkedIn feed.
+// @author       Jabed Miah
+// @match        *.linkedin.com/feed/
+// @icon         https://www.google.com/s2/favicons?domain=linkedin.com
+// @supportURL   https://github.com/miahj1/linkedin-promoted-content-blocker/issues
+// @license      MIT
+// ==/UserScript==
+
+(new MutationObserver(elementCheck)).observe(document, {childList: true, subtree: true});
+
+function elementCheck(changes, observer) {
+    const cards = document.getElementsByClassName("ember-view  occludable-update");
+
+    for (card of cards) {
+        const companyFound = card.querySelector('img[src*="company-logo_100_100"]')
+        if (companyFound) {
+            card.style.display = "none";
+        }
+    }
+}
