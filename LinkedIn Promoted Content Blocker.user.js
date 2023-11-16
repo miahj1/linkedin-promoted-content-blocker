a3593558f9eb7a28ec1d8e755a3978dd59b860a7
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-11-16 00:29:55 -0500

    Update LinkedIn Promoted Content Blocker.user.js

--- LinkedIn Promoted Content Blocker.user.js
+++ LinkedIn Promoted Content Blocker.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         LinkedIn Promoted Content Blocker
 // @namespace    https://www.jabedmiah.com
-// @version      1.01
+// @version      1.02
 // @description  Blocks promoted posts from the LinkedIn feed.
 // @author       Jabed Miah
-// @match        *.linkedin.com/feed/*
+// @match        *://*.linkedin.com/*
 // @icon         https://www.google.com/s2/favicons?domain=linkedin.com
 // @supportURL   https://github.com/miahj1/linkedin-promoted-content-blocker/issues
 // @license      MIT
@@ -12,13 +12,17 @@
 
 (new MutationObserver(elementCheck)).observe(document, {childList: true, subtree: true});
 
-function elementCheck(changes, observer) {
-    const cards = document.getElementsByClassName("ember-view  occludable-update");
+let initialUrl = location.href;
 
-    for (card of cards) {
-        const companyFound = card.querySelector('img[src*="company-logo_100_100"]')
-        if (companyFound) {
-            card.style.display = "none";
+function elementCheck(changes, observer) {
+    if (initialUrl.includes("feed")){
+        const cards = document.getElementsByClassName("ember-view  occludable-update");
+
+        for (card of cards) {
+            const companyFound = card.querySelector('img[src*="company-logo_100_100"]')
+            if (companyFound) {
+                card.style.display = "none";
+            }
         }
     }
 }
